3bb5ef8658e8153ee0427c6f0d078f673bc86ed3
Rafał <r.furmanski@gmail.com>
2014-09-03 15:43:16 +0200

    Update runtests.py

--- testproject/runtests.py
+++ testproject/runtests.py
@@ -8,7 +8,14 @@ import os
 def run_tests():
 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testproject.settings")
-
+    
+    try:
+        # Django 1.7 specific
+        import django
+        django.setup()
+    except:
+        pass
+    
     from django.test.runner import DiscoverRunner
     test_runner = DiscoverRunner(
         verbosity=1, interactive=False, failfast=False)
