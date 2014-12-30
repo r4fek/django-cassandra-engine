a7b5131b64a653cc100353b328642b13b114aa88
Rafał Furmański <r.furmanski@gmail.com>
2014-12-30 10:52:01 +0100

    Move syncing code to sync_cassandra command
    Also update .travis.yml to run only on Cassandra 2.0.

--- django_cassandra_engine/management/commands/sync_cassandra.py
+++ django_cassandra_engine/management/commands/sync_cassandra.py
@@ -1,25 +1,83 @@
-from django.core.management.base import NoArgsCommand
+from optparse import make_option
+import django
+from django.core.management.base import NoArgsCommand, CommandError
 from django.db import connections
+from django.conf import settings
 
-from django_cassandra_engine.management.commands import syncdb
+from cqlengine.management import create_keyspace, sync_table
 
 
 class Command(NoArgsCommand):
+    option_list = NoArgsCommand.option_list + (
+        make_option('--database', action='store', dest='database',
+                    default=None, help='Nominates a database to synchronize.'),
+    )
 
-    help = 'Sync Cassandra database'
+    help = 'Sync Cassandra database(s)'
+
+    @staticmethod
+    def _import_management():
+        """
+        Import the 'management' module within each installed app, to register
+        dispatcher events.
+        """
+
+        from django.utils.importlib import import_module
+
+        for app_name in settings.INSTALLED_APPS:
+            try:
+                import_module('.management', app_name)
+            except ImportError as exc:
+                # This is slightly hackish. We want to ignore ImportErrors
+                # if the "management" module itself is missing -- but we don't
+                # want to ignore the exception if the management module exists
+                # but raises an ImportError for some reason. The only way we
+                # can do this is to check the text of the exception. Note that
+                # we're a bit broad in how we check the text, because different
+                # Python implementations may not use the same text.
+                # CPython uses the text "No module named management"
+                # PyPy uses "No module named myproject.myapp.management"
+                msg = exc.args[0]
+                if not msg.startswith('No module named') \
+                        or 'management' not in msg:
+                    raise
+
+    def sync(self, alias):
+        engine = settings.DATABASES.get(alias, {}).get('ENGINE', '')
+
+        if engine != 'django_cassandra_engine':
+            raise CommandError('Database {} is not cassandra!'.format(alias))
+
+        connection = connections[alias]
+        connection.connect()
+        options = connection.settings_dict.get('OPTIONS', {})
+        replication_opts = options.get('replication', {})
+        keyspace = connection.settings_dict['NAME']
+
+        self.stdout.write('Creating keyspace {}..'.format(keyspace))
+        create_keyspace(keyspace, **replication_opts)
+        for app_name, app_models \
+                in connection.introspection.cql_models.iteritems():
+            for model in app_models:
+                self.stdout.write('Syncing %s.%s' % (app_name, model.__name__))
+                sync_table(model, create_missing_keyspace=False)
 
     def handle_noargs(self, **options):
-        cassandra_alias = None
 
+        if django.VERSION < (1, 7):
+            self._import_management()
+
+        database = options.get('database')
+        if database is not None:
+            return self.sync(database)
+
+        cassandra_alias = None
         for alias in connections:
             engine = connections[alias].settings_dict.get('ENGINE', '')
             if engine == 'django_cassandra_engine':
+                self.sync(alias)
                 cassandra_alias = alias
 
         if cassandra_alias is None:
-            self.stderr.write(
+            raise CommandError(
                 'Please add django_cassandra_engine backend to DATABASES!')
-            return
-
-        options['database'] = options.get('database', cassandra_alias)
-        syncdb.Command().execute(**options)
--- django_cassandra_engine/management/commands/syncdb.py
+++ django_cassandra_engine/management/commands/syncdb.py
@@ -1,39 +1,10 @@
-import django
 from django.conf import settings
 from django.core.management.commands.syncdb import Command as SyncCommand
-from django.db import connections
 
-from cqlengine.management import create_keyspace, sync_table
+from django_cassandra_engine.management.commands import sync_cassandra
 
 
 class Command(SyncCommand):
-
-    @staticmethod
-    def _import_management():
-        """
-        Import the 'management' module within each installed app, to register
-        dispatcher events.
-        """
-
-        from django.utils.importlib import import_module
-
-        for app_name in settings.INSTALLED_APPS:
-            try:
-                import_module('.management', app_name)
-            except ImportError as exc:
-                # This is slightly hackish. We want to ignore ImportErrors
-                # if the "management" module itself is missing -- but we don't
-                # want to ignore the exception if the management module exists
-                # but raises an ImportError for some reason. The only way we
-                # can do this is to check the text of the exception. Note that
-                # we're a bit broad in how we check the text, because different
-                # Python implementations may not use the same text.
-                # CPython uses the text "No module named management"
-                # PyPy uses "No module named myproject.myapp.management"
-                msg = exc.args[0]
-                if not msg.startswith('No module named') \
-                        or 'management' not in msg:
-                    raise
 
     def handle_noargs(self, **options):
         db = options.get('database')
@@ -42,21 +13,5 @@ class Command(SyncCommand):
         # Call regular syncdb if engine is different from ours
         if engine != 'django_cassandra_engine':
             return super(Command, self).handle_noargs(**options)
-
-        if django.VERSION < (1, 7):
-            self._import_management()
-
-        connection = connections[db]
-        connection.connect()
-        options = connection.settings_dict.get('OPTIONS', {})
-        replication_opts = options.get('replication', {})
-        keyspace = connection.settings_dict['NAME']
-
-        self.stdout.write('Creating keyspace %s..' % keyspace)
-        create_keyspace(keyspace, **replication_opts)
-        for app_name, app_models \
-                in connection.introspection.cql_models.iteritems():
-
-            for model in app_models:
-                self.stdout.write('Syncing %s.%s' % (app_name, model.__name__))
-                sync_table(model, create_missing_keyspace=False)
+        else:
+            return sync_cassandra.Command().execute(**options)
