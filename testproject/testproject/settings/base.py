67762f4c790f912abf3cc8c6f6ebb85975040877
Rafał Furmański <r.furmanski@gmail.com>
2014-12-30 10:45:09 +0100

    Separate settings modules to make testing easier

--- testproject/testproject/settings/base.py
+++ testproject/testproject/settings/base.py
@@ -56,38 +56,6 @@ ROOT_URLCONF = 'testproject.urls'
 WSGI_APPLICATION = 'testproject.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'cassandra': {
-        'ENGINE': 'django_cassandra_engine',
-        'NAME': 'db',
-        'USER': 'user',
-        'PASSWORD': 'pass',
-        'TEST_NAME': 'test_db',
-        'HOST': '127.0.0.1',
-        'OPTIONS': {
-            'replication': {
-                'strategy_class': 'SimpleStrategy',
-                'replication_factor': 1,
-            },
-            'connection': {
-                'lazy_connect': True,
-                'retry_connect': True,
-                'consistency': ConsistencyLevel.ALL
-            },
-            'session': {
-                'default_timeout': 15
-            }
-        }
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
@@ -106,3 +74,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+TEST_RUNNER = 'djangotoolbox.test.CapturingTestSuiteRunner'
---
+++ testproject/testproject/settings/default_cassandra.py
@@ -0,0 +1,32 @@
+from base import *
+
+# THIS CONFIGURATION REQUIRES CUSTOM DATABASE ROUTER
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django_cassandra_engine',
+        'NAME': 'db',
+        'USER': 'user',
+        'PASSWORD': 'pass',
+        'TEST_NAME': 'test_db',
+        'HOST': '127.0.0.1',
+        'OPTIONS': {
+            'replication': {
+                'strategy_class': 'SimpleStrategy',
+                'replication_factor': 1,
+            },
+            'connection': {
+                'lazy_connect': True,
+                'retry_connect': True,
+                'consistency': ConsistencyLevel.ALL
+            },
+            'session': {
+                'default_timeout': 15
+            }
+        }
+    },
+    'other': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+}
