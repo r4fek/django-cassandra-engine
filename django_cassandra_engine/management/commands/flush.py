13b3e663ad87306557cb4d1ad104a2f71400393e
Rafał Furmański <r.furmanski@gmail.com>
2015-01-19 10:05:52 +0100

    Fix flush command

--- django_cassandra_engine/management/commands/flush.py
+++ django_cassandra_engine/management/commands/flush.py
@@ -12,3 +12,17 @@ class Command(FlushCommand):
             options['inhibit_post_migrate'] = True
 
         return super(Command, self).handle_noargs(**options)
+
+    @staticmethod
+    def emit_post_syncdb(verbosity, interactive, database):
+        engine = get_engine_from_db_alias(database)
+        if engine != 'django_cassandra_engine':
+            return FlushCommand.emit_post_syncdb(verbosity, interactive,
+                                                 database)
+
+    @staticmethod
+    def emit_post_migrate(verbosity, interactive, database):
+        engine = get_engine_from_db_alias(database)
+        if engine != 'django_cassandra_engine':
+            return FlushCommand.emit_post_migrate(verbosity, interactive,
+                                                  database)
