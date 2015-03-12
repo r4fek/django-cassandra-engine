832bedd85d1401375ee456c3a414629b0659086b
Rafał Furmański <rfurmanski@opera.com>
2015-03-12 08:12:23 +0100

    Revert poor #22 fix (for now)

--- django_cassandra_engine/management/commands/runserver.py
+++ django_cassandra_engine/management/commands/runserver.py
@@ -1,10 +1,7 @@
-from django.core.management import get_commands
+from django.core.management.commands.runserver import Command as RunserverCmd
 from django.db import DEFAULT_DB_ALIAS
 
 from django_cassandra_engine.utils import get_engine_from_db_alias
-
-
-RunserverCmd = get_commands()['runserver']
 
 
 class Command(RunserverCmd):
