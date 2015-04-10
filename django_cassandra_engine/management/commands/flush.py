1e7d09815634035fd4e8da51a6e154f7d08d5757
Rafał Furmański <rfurmanski@opera.com>
2015-04-10 13:09:16 +0200

    Disable interactive mode when flushing db

--- django_cassandra_engine/management/commands/flush.py
+++ django_cassandra_engine/management/commands/flush.py
@@ -8,16 +8,22 @@ class Command(FlushCommand):
     def handle_noargs(self, **options):
         engine = get_engine_from_db_alias(options['database'])
         if engine == 'django_cassandra_engine':
-            options['load_initial_data'] = False
-            options['inhibit_post_migrate'] = True
+            options.update({
+                'load_initial_data': False,
+                'interactive': False,
+                'inhibit_post_migrate': True
+            })
 
         return super(Command, self).handle_noargs(**options)
 
     def handle(self, **options):
         engine = get_engine_from_db_alias(options['database'])
         if engine == 'django_cassandra_engine':
-            options['load_initial_data'] = False
-            options['inhibit_post_migrate'] = True
+            options.update({
+                'load_initial_data': False,
+                'interactive': False,
+                'inhibit_post_migrate': True
+            })
 
         return super(Command, self).handle(**options)
 
