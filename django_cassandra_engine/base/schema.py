962ce958ae1ca2a8555d3094d125032e1badc050
Rafał Furmański <rfurmanski@opera.com>
2016-03-17 08:50:39 +0100

    Fix #22: Do not override default runserver command

--- django_cassandra_engine/base/schema.py
+++ django_cassandra_engine/base/schema.py
@@ -8,10 +8,21 @@ except ImportError:
 
 
 class CassandraDatabaseSchemaEditor(BaseDatabaseSchemaEditor):
-    # TODO: support cql statements
+
+    def prepare_default(self, value):
+        return value
+
+    def quote_value(self, value):
+        return value
 
     def create_model(self, model):
-        pass
+        # Hack to disable checking migrations for cassandra connection
+        try:
+            from django.db.migrations.exceptions import (
+                MigrationSchemaMissing as Exc)
+        except ImportError:
+            from django.core.exceptions import ImproperlyConfigured as Exc
+        raise Exc('No schema for cassandra database')
 
     def delete_model(self, model):
         pass
