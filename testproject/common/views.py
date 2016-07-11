f71644f5fb5a690e0551c11aeebb619de41d2536
grillazz <the@grillazz.com>
2016-07-11 12:23:51 +0200

    update views

--- testproject/common/views.py
+++ testproject/common/views.py
@@ -1,25 +1,31 @@
-from rest_framework import viewsets
+from rest_framework.viewsets import ViewSet, ModelViewSet
 from rest_framework.generics import ListCreateAPIView, ListAPIView
 from rest_framework.response import Response
 
-from .models import CassandraThingMultiplePK
-from .serializers import ThingSerializer
+from .models import CassandraThingMultiplePK, CassandraThing
+from .serializers import ThingSerializer, ThingMultiplePKSerializer
 
 
-class ThingViewSet(viewsets.ViewSet):
+class ThingMultiplePKViewSet(ViewSet):
     def list(self, request):
         queryset = CassandraThingMultiplePK.objects.all()
-        serializer = ThingSerializer(queryset, many=True)
+        serializer =ThingMultiplePKSerializer(queryset, many=True)
         return Response(serializer.data)
 
 
-class ThingListCreateAPIView(ListCreateAPIView):
+class ThingMultiplePKListCreateAPIView(ListCreateAPIView):
     queryset = CassandraThingMultiplePK.objects.all()
-    serializer_class = ThingSerializer
+    serializer_class = ThingMultiplePKSerializer
     permission_classes = ()
 
 
-class ThingListAPIView(ListAPIView):
+class ThingMultiplePKListAPIView(ListAPIView):
     queryset = CassandraThingMultiplePK.objects.all()
+    serializer_class = ThingMultiplePKSerializer
+    permission_classes = ()
+
+
+class CassandraThing2ViewSet(ModelViewSet):
     serializer_class = ThingSerializer
+    queryset = CassandraThing.objects.all()
     permission_classes = ()
