5119e9a71bc59f957438eae28cce89928814b069
grillazz <the@grillazz.com>
2016-07-11 16:58:54 +0200

    fix unit tests

--- testproject/common/tests/test_drf.py
+++ testproject/common/tests/test_drf.py
@@ -1,30 +1,29 @@
+import uuid
+
 from rest_framework.test import APITestCase
 
 from common.models import CassandraThing
 
 
-class CreateReadThingTest1(APITestCase):
-    def setUp(self):
-        self.data = {'id': 'a9be910b-3338-4340-b773-f7ec2bc1ce1a', 'data_abstract': 'TeXt'}
+class CreateReadThingTest(APITestCase):
 
-    def test_create_thing(self):
-        response = self.client.post('/common/thing-modelviewset/', self.data, format='json')
+    def setUp(self):
+        self.data2a = {'id': 'a9be910b-3338-4340-b773-f7ec2bc1ce1a', 'data_abstract': 'TeXt'}
+        self.data2b = {'id': 'a9be910b-3338-4340-b773-f7ec2bc1ce1b', 'data_abstract': 'TeXt'}
+
+    def test_create_thing2a(self):
+        response = self.client.post('/common/thing-modelviewset/', self.data2a, format='json')
         self.assertEqual(response.status_code, 201)
         self.assertEqual(CassandraThing.objects.count(), 1)
-        self.assertEqual(CassandraThing.objects.get().id, 'a9be910b-3338-4340-b773-f7ec2bc1ce1a')
+        self.assertEqual(CassandraThing.objects.get().id, uuid.UUID('a9be910b-3338-4340-b773-f7ec2bc1ce1a'))
 
-
-class CreateReadThingTest2(APITestCase):
-    def setUp(self):
-        self.data = {'id': 'a9be910b-3338-4340-b773-f7ec2bc1ce1b', 'data_abstract': 'TeXt'}
-
-    def test_create_thing(self):
-        response = self.client.post('/common/thing-modelviewset/', self.data, format='json')
+    def test_create_thing2b(self):
+        response = self.client.post('/common/thing-modelviewset/', self.data2b, format='json')
         self.assertEqual(response.status_code, 201)
         self.assertEqual(CassandraThing.objects.count(), 2)
         self.assertEqual(CassandraThing.objects.get(id='a9be910b-3338-4340-b773-f7ec2bc1ce1b').id,
-                         'a9be910b-3338-4340-b773-f7ec2bc1ce1b')
-        response = self.client.get('/common/thing-modelviewset/a9be910b-3338-4340-b773-f7ec2bc1ce1b/', format='json')
+                         uuid.UUID('a9be910b-3338-4340-b773-f7ec2bc1ce1b'))
+        response = self.client.get('/api/thing-modelviewset4/a9be910b-3338-4340-b773-f7ec2bc1ce1b/', format='json')
         self.assertEqual(response.status_code, 200)
 
 
