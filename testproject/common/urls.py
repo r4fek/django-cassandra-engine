95dd4ecd3387331d1eb0159a3f389bf4bca3fbce
grillazz <the@grillazz.com>
2016-07-11 12:51:49 +0200

    update urls

--- testproject/common/urls.py
+++ testproject/common/urls.py
@@ -1,9 +1,14 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
+from rest_framework import routers
 
-from .views import ThingViewSet, ThingListCreateAPIView, ThingListAPIView
+from .views import ThingMultiplePKViewSet, ThingMultiplePKListCreateAPIView, ThingMultiplePKListAPIView, ThingModelViewSet
+
+router = routers.DefaultRouter()
+router.register(r'thing-modelviewset', ThingModelViewSet)
 
 urlpatterns = [
-    url(r'^thing-viewset/$', ThingViewSet.as_view({'get': 'list'}), name='thing_viewset_api'),
-    url(r'^thing-listcreate/$', ThingListCreateAPIView.as_view(), name='thing_listcreate_api'),
-    url(r'^thing-listview/$', ThingListAPIView.as_view(), name='thing_listview_api'),
+    url(r'^thing-viewset/$', ThingMultiplePKViewSet.as_view({'get': 'list'}), name='thing_viewset_api'),
+    url(r'^thing-listcreate/$', ThingMultiplePKListCreateAPIView.as_view(), name='thing_listcreate_api'),
+    url(r'^thing-listview/$', ThingMultiplePKListAPIView.as_view(), name='thing_listview_api'),
+    url(r'^', include(router.urls)),
 ]
