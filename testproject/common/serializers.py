50d89da56127aa90cb68ef5b27a7ed6da19c9629
grillazz <the@grillazz.com>
2016-07-11 12:03:35 +0200

    update serializers

--- testproject/common/serializers.py
+++ testproject/common/serializers.py
@@ -1,10 +1,17 @@
 from django_cassandra_engine.serializers import DjangoCassandraModelSerializer
 
-from common.models import CassandraThingMultiplePK
+from common.models import CassandraThingMultiplePK, CassandraThing
+
+
+class ThingMultiplePKSerializer(DjangoCassandraModelSerializer):
+
+    class Meta:
+        model = CassandraThingMultiplePK
+        fields = '__all__'
 
 
 class ThingSerializer(DjangoCassandraModelSerializer):
 
     class Meta:
-        model = CassandraThingMultiplePK
+        model = CassandraThing
         fields = '__all__'
