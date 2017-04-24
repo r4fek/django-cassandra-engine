be330c8f79ea4860a43d2ba09847080571dd9133
Rafał Furmański <rfurmanski@opera.com>
2017-04-24 13:42:29 +0200

    Fix #90: Remove not needed `construct_instance` monkey patching

--- django_cassandra_engine/apps.py
+++ django_cassandra_engine/apps.py
@@ -1,58 +1,47 @@
 from __future__ import absolute_import, unicode_literals
 
-import inspect
 from django.apps import AppConfig as DjangoAppConfig
-import django.forms.models
+import cassandra.cqlengine.columns
 
 
-def construct_instance(form, instance, fields=None, exclude=None):
+class CallableBool:
     """
-    Taken from django.forms.models.construct_instance
-    altered to support cqlengine models (see field_has_default logic)
+    An boolean-like object that is also callable for backwards compatibility.
     """
-    from django.db import models
-    opts = instance._meta
 
-    cleaned_data = form.cleaned_data
-    file_field_list = []
-    for f in opts.fields:
-        if not f.editable or isinstance(f, models.AutoField) \
-                or f.name not in cleaned_data:
-            continue
-        if fields is not None and f.name not in fields:
-            continue
-        if exclude and f.name in exclude:
-            continue
-        # Leave defaults for fields that aren't in POST data, except for
-        # checkbox inputs because they don't appear in POST data if not checked
+    def __init__(self, value):
+        self.value = value
 
-        # cqlengine support logic start
-        if inspect.ismethod(f.has_default):
-            # django model
-            field_has_default = f.has_default()
-        else:
-            # cqlengine model
-            field_has_default = f.has_default
-        # cqlengine support logic end
+    def __bool__(self):
+        return self.value
 
-        if (field_has_default and form.add_prefix(f.name) not in form.data and
-            not getattr(form[f.name].field.widget,
-                        'dont_use_model_field_default_for_empty_data', False)):
-            continue
-        # Defer saving file-type fields until after the other fields, so a
-        # callable upload_to can use the values from other fields.
-        if isinstance(f, models.FileField):
-            file_field_list.append(f)
-        else:
-            f.save_form_data(instance, cleaned_data[f.name])
+    def __call__(self):
+        return self.value
 
-    for f in file_field_list:
-        f.save_form_data(instance, cleaned_data[f.name])
+    def __nonzero__(self):  # Python 2 compatibility
+        return self.value
 
-    return instance
+    def __repr__(self):
+        return 'CallableBool(%r)' % self.value
 
-# patching so that Django ModelForms can work with cqlengine models
-django.forms.models.construct_instance = construct_instance
+    def __eq__(self, other):
+        return self.value == other
+
+    def __ne__(self, other):
+        return self.value != other
+
+    def __or__(self, other):
+        return bool(self.value or other)
+
+    def __hash__(self):
+        return hash(self.value)
+
+
+def has_default(self):
+    return CallableBool(self.default is not None)
+
+# monkey patch Column.has_default to be able to use function call too
+cassandra.cqlengine.columns.Column.has_default = property(has_default)
 
 
 class AppConfig(DjangoAppConfig):
